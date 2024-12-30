9fe68ce4b0095de49efaed1602f53d855739e1b0
kameliaptr <kameliaputri2516@gmail.com>
2024-12-30 23:47:45 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Include komponen template
+include 'includes/head.php';
+include 'includes/header.php';
+include 'includes/navbar.php';
+include 'content.php';
+include 'includes/footer.php';
+?>
